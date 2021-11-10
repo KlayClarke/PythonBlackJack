e4cda74293dcf0d32cf9bcace9a2daef45a7834f
username <clarkeklay@gmail.com>
2021-11-10 15:13:03 -0500

    write code to account for soft 17 rules, solved minor bugs that return bad values

--- blackjack.py
+++ blackjack.py
@@ -7,26 +7,24 @@ import random
 def check_for_ace():
     for card in user_hand:
         if card == 11:
+            print(sum(user_hand))
             if sum(user_hand) < 21:
-                print('You\'ve been dealt an ace! \n')
+                print('\nYou\'ve been dealt an ace! \n')
                 ace_choice = int(input('\nWould you like this ace to count as 1 or 11? Please type either "1" or "11" to continue. \n' ))
                 if ace_choice == 1:
                     card_index = user_hand.index(card)
                     user_hand[card_index] = 1
                     user_score = sum(user_hand)
-                    print(f'\nYour cards: {user_hand}, current score: {user_score}')
+                    print(f'\nYour cards: {user_hand}, your score: {user_score}')
                 elif ace_choice == 11:
                     card_index = user_hand.index(card)
                     user_hand[card_index] = 11
                     user_score = sum(user_hand)
-                    print(f'\nYour cards: {user_hand}, current score: {user_score}')
+                    print(f'\nYour cards: {user_hand}, your score: {user_score}')
             elif sum(user_hand) > 21:
-                print(f'\nYour final hand: {user_hand}, final score: {user_score}\n')
-                print(f'\nDealer\'s final hand: {dealer_hand}, final score: {dealer_score}\n')
+                print(f'\nYour hand: {user_hand}, your score: {user_score}\n')
+                print(f'\nDealer\'s hand: {dealer_hand}, dealer\'s score: {dealer_score}\n')
                 print('\nDealer Wins\n')
-            else:
-                continue
-
 ##def soft_17():
 ##    if dealer_score >= 17 or dealer_score == 21:
 ##        end_of_game = True
@@ -35,34 +33,33 @@ def check_for_ace():
 ##            dealer_hand.append(random.choice(cards))
 ##            dealer_score = sum(dealer_hand)
 def show_score():
-    print(f'\nYour final hand: {user_hand}, final score: {user_score}\n')
-    print(f'\nDealer\'s final hand: {dealer_hand}, final score: {dealer_score}\n')
+    print(f'\nYour hand: {user_hand}, your score: {user_score}\n')
+    print(f'\nDealer\'s hand: {dealer_hand}, dealer\'s score: {dealer_score}\n')
     
 end_of_game = False
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 user_hand = []
 dealer_hand = []
-user_score = 0
-dealer_score = 0
 
 user_hand.append(random.choice(cards))
+dealer_hand.append(random.choice(cards))
 dealer_hand.append(random.choice(cards))
 
 while not end_of_game:
 
     user_hand.append(random.choice(cards))
+
     user_score = sum(user_hand)
 
     print(f'\nYour cards: {user_hand}, current score: {user_score}\n')
                   
     check_for_ace()
-    
-    dealer_hand.append(random.choice(cards))
+
     dealer_score = sum(dealer_hand)
 
     print(f'\nDealer\'s first card: {dealer_hand[0]}\n')
 
-    if user_score >= 21:
+    if user_score >= 21 or dealer_score >= 21:
         end_of_game = True
     else:
         user_hit = input('\nType "y" to get another card, type "n" to pass: \n').lower()
@@ -70,8 +67,12 @@ while not end_of_game:
             end_of_game = False  
 
         elif user_hit == 'n':
-            #try to account for soft 17 rule of dealers... once dealer soft 17, then compare hands and reveal winner
-            end_of_game = True
+            if dealer_score < 17:
+                while dealer_score < 17:                
+                    dealer_hand.append(random.choice(cards))
+                    dealer_score = sum(dealer_hand)
+                    show_score()
+                end_of_game = True
         
 if end_of_game == True:
     if dealer_score > 21 and user_score > 21:
